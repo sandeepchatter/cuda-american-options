2f5a6852a6ed255136ae4ec5a03c3cc289b53df4
ilonks@gmail.com <ilonks@gmail.com@a1d2ec09-dbfa-08db-2cb4-14d8b5a00299>
2013-11-28 02:37:38 +0000

    Changing up the files a bit. GPU is still not fixed.

--- trunk/european_gpu.cu
+++ trunk/european_gpu.cu
@@ -89,65 +89,10 @@ static __global__ void MonteCarloOneBlockPerOption(
     }
 }
 
-
-
-
-
-
-
-float randFloat(float low, float high)
-{
-    float t = (float)rand() / (float)RAND_MAX;
-    return (1.0f - t) * low + t * high;
-}
-
-
-
-void MonteCarloGPU(TOptionPlan *plan, cudaStream_t stream);
-
-///////////////////////////////////////////////////////////////////////////////
-// CPU reference functions
-///////////////////////////////////////////////////////////////////////////////
-extern "C" void MonteCarloCPU(
-    TOptionValue   &callValue,
-    TOptionData optionData,
-    float *h_Random,
-    int pathN
-);
-
-int main(void) {
-
-    TOptionData optionData;
-    TOptionValue callValueGPU;
-    TOptionValue callValueCPU;
-    int PATH_N = 262133;
-    optionData.S = randFloat(5.0f, 50.0f);
-    optionData.X = randFloat(10.0f, 25.0f);
-    optionData.T = randFloat(1.0f, 5.0f);
-    optionData.R = 0.06f;
-    optionData.V = 0.10f;
-    callValueGPU.Expected   = -1.0f;
-    callValueGPU.Confidence = -1.0f;
-
-    MonteCarloCPU(
-            callValueCPU,
-            optionData,
-            NULL,
-            PATH_N
-            );
-
-    printf("Expected: %f\n", callValueCPU.Expected);
-    printf("Confidence: %f\n", callValueCPU.Confidence);
-
-
-    return 0;
-}
-
-
 //Main computations
-void MonteCarloGPU(TOptionPlan *plan, cudaStream_t stream)
+extern "C" void MonteCarloGPU(TOptionPlan *plan, cudaStream_t stream)
 {
-    __TOptionData h_OptionData[MAX_OPTIONS];
+  /*  __TOptionData h_OptionData[MAX_OPTIONS];
     __TOptionValue *h_CallValue = plan->h_CallValue;
 
     if (plan->optionCount <= 0 || plan->optionCount > MAX_OPTIONS)
@@ -189,6 +134,7 @@ void MonteCarloGPU(TOptionPlan *plan, cudaStream_t stream)
                         plan->optionCount * sizeof(__TOptionValue), (size_t)0, cudaMemcpyDeviceToHost, stream
                     ));
 
+*/
     //cudaDeviceSynchronize();
 
 }
